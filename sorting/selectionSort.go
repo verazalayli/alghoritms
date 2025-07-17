d732a21e423e650605ae298e6737bedcc9b8be73
Ilya Lazarev <ilya-lazarev3012@yandex.ru>
2025-07-17 22:27:20 +0300

    selection sort + insertion sort

---
+++ sorting/selectionSort.go
@@ -0,0 +1,20 @@
+package sorting
+
+func SelectionSort(arr []int) []int {
+	sorted := make([]int, len(arr))
+	length := len(arr)
+	for i := 0; i < length; i++ {
+		minIndex := i
+		for j := i + 1; j < length; j++ {
+			if arr[minIndex] > arr[j] {
+				minIndex = j
+				sorted = append(sorted, arr[minIndex])
+				arr = arr[:minIndex]
+			}
+		}
+		if minIndex != i {
+			arr[i], arr[minIndex] = arr[minIndex], arr[i]
+		}
+	}
+	return arr
+}
