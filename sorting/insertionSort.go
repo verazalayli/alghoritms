6e106d4e3fed3478e828e59d81b009c6ab402a6a
macbookair <imlazarev@phoenixit.ru>
2025-07-17 22:27:20 +0300

    selection sort + insertion sort

---
+++ sorting/insertionSort.go
@@ -0,0 +1,12 @@
+package sorting
+
+func InsertionSort(arr []int) []int {
+	for i := 1; i < len(arr); i++ {
+		for j := i; j < len(arr); j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	return arr
+}
